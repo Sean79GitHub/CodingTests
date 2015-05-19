148b255a2dd8dcb86447402088f8f51e01416065
Sean79GitHub <seanmccann@hotmail.com>
2015-05-19 16:47:35 +0100

    Committed in error

--- TemperatureConverter/TemperatureConverterDomain/Interfaces/ILanguageSettingsManager.cs
+++
@@ -1,7 +0,0 @@
-namespace TemperatureConverterDomain.Interfaces
-{
-    public interface ILanguageSettingsManager
-    {
-        void ChangeLanguage(string language);
-    }
-}
--- TemperatureConverter/TemperatureConverterDomain/Interfaces/ITemperatureConverterCalculator.cs
+++
@@ -1,8 +0,0 @@
-namespace TemperatureConverterDomain.Interfaces
-{
-    public interface ITemperatureConverterCalculator
-    {
-        double ConvertCelsiusToFahrenheit(double celsiusValue);
-        double ConvertFahrenheitToCelsius(double fahrenheitValue);
-    }
-}
